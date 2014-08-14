5f43758a45baab1fbc2221800a7e19e79341fba3
h43z <h43zet@gmail.com>
2014-08-14 10:39:17 -0500

    removed terminator due to incompatible configs between lappis

--- setup.sh
+++ setup.sh
@@ -3,4 +3,3 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sf $PWD/.vimrc ~/.vimrc 
 ln -sf $PWD/.conkyrc ~/.conkyrc
 ln -sf $PWD/.gmrunrc ~/.gmrunc 
-ln -sf $PWD/terminator .config/terminator/config
