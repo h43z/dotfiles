757aadc3bf3a7d8859a4b7c58e9585d580ad1c49
h43z <h43zet@gmail.com>
2014-08-19 23:33:19 +0200

    using omz now

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+#install various plugins
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir ~/.vim/undodir
+mkdir -p ~/.vim/undodir
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+#symlink my own dotfiles
 ln -sf $PWD/.vimrc ~/.vimrc 
 ln -sf $PWD/.conkyrc ~/.conkyrc
 ln -sf $PWD/.zshrc ~/.zshrc
