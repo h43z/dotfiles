94e7feb53c50651aaf980d43121bd24e301aee8b
h43z <h43zet@gmail.com>
2014-08-19 18:49:04 +0200

    added setup entry

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir ~/.vim/undodir
 ln -sf $PWD/.vimrc ~/.vimrc 
 ln -sf $PWD/.conkyrc ~/.conkyrc
+ln -sf $PWD/.zshrc ~/.zshrc
