553762a40e5266624f0a520939ceedb85168043f
h43z <h43zet@gmail.com>
2015-02-23 22:49:35 +0100

    removed unnessesarljffkajsy line

--- setup.sh
+++ setup.sh
@@ -13,7 +13,6 @@ ln -sf $PWD/conkyrc ~/.conkyrc
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
 
-source ~/.zsh/git-prompt/zshrc.sh
 vim -i NONE -c VundleUpdate -c quitall
 
 echo "All dotfiles are in place"
