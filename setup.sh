baa3709282075455434df4becef54c73cce5d265
h43z <h43zet@gmail.com>
2015-02-23 22:57:10 +0100

    added htoprc

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ git clone https://github.com/olivierverdier/zsh-git-prompt.git ~/.zsh/git-prompt
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/syntax-highlighting
 
 mkdir -p ~/.config/terminator/
+mkdir -p ~/.config/htop/
 mkdir -p ~/.vim/undodir
 
 ln -sf $PWD/vimrc ~/.vimrc
@@ -12,6 +13,7 @@ ln -sf $PWD/terminator.conf ~/.config/terminator/config
 ln -sf $PWD/conkyrc ~/.conkyrc
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/tmux.conf ~/.tmux.conf
+ln -sf $PWD/htoprc ~/.config/htop/htoprc
 
 vim -i NONE -c VundleUpdate -c quitall
 
